aff5a943f2592f231317c132b2bedae3241a9318
leah <leahi2025@gmail.com>
2025-05-19 15:07:44 -0700

    signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login
 from supabase import create_client
 from django.shortcuts import render, redirect
 from django.conf import settings
@@ -12,3 +13,21 @@ def signup(request):
         result = supabase.auth.sign_up({"email": email, "password": password})
         return redirect("login")
     return render(request, "signup.html")
+
+
+def signin(request):
+    if request.method == "POST":
+        email = request.POST["email"]
+        password = request.POST["password"]
+        auth = supabase.auth.sign_in({"email": email, "password": password})
+        token = auth.get("session", {}).get("access_token")
+        if not token:
+            # handle invalid creds
+            return render(request, "signin.html", {"error": "Invalid"})
+        # Authenticate via our backend
+        user = authenticate(request, token=token)
+        if user:
+            login(request, user)  # creates a Django session
+            request.session["supabase_jwt"] = token
+            return redirect("dashboard")
+    return render(request, "signin.html")
